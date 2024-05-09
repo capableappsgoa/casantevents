1f0b585db437c84aa1ddde5019e951e923f4e9e7
emperifybusiness <149296329+emperifybusiness@users.noreply.github.com>
2024-05-09 10:51:13 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import Modal from "react-modal";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./index.css";
 import { NumberProvider } from './pages/Context';
 Modal.setAppElement("#root");
@@ -9,7 +10,9 @@ Modal.setAppElement("#root");
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
+    <SpeedInsights>
     <NumberProvider>
         <App />
     </NumberProvider>
+    </SpeedInsights>
 );
