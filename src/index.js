354f69e4abeeb44978829202273e8f8ecb4b2206
emperifybusiness <149296329+emperifybusiness@users.noreply.github.com>
2024-05-09 10:51:59 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import Modal from "react-modal";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./index.css";
 import { NumberProvider } from './pages/Context';
 Modal.setAppElement("#root");
