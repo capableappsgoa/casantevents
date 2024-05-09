4578b12f037b74b8c298822e9dd0b2842040ff73
emperifybusiness <149296329+emperifybusiness@users.noreply.github.com>
2024-05-09 11:31:50 +0530

    hi

--- src/PrivateRoutes.js
+++
@@ -1,9 +0,0 @@
-import {Outlet, Navigate} from 'react-router-dom'
-import React , {useState} from 'react'
-import Login from './pages/Login'
-export const PrivateRoutes = () => {
-  const userinfo = true
-  return userinfo ? <Navigate to="/admin" /> : <Navigate to="/login" />
-}
-
-
--- src/pages/PrivateRoutes.js
+++
@@ -1,8 +0,0 @@
-import {Outlet, Navigate} from 'react-router-dom'
-
-import React from 'react'
-
-export const PrivateRoutes = () => {
-const user = true;
-return user? <Outlet /> : <Navigate to="/OtherAdmins" />
-}
